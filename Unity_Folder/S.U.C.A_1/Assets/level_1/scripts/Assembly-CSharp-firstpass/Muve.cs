20083f31bb1a4071edacb16a2c71a9f44fa12d86
NazarkoNaN <nazar.ivanyuk@gmail.com>
2021-08-02 17:09:46 +0300

    Додав UI

---
+++ Unity_Folder/S.U.C.A_1/Assets/level_1/scripts/Assembly-CSharp-firstpass/Muve.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muve : MonoBehaviour
+{
+   public void MoveToObject()
+    {
+
+    }
+
+    public void MoveObject()
+    {
+
+    }
+
+
+}
